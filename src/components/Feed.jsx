dafd5d5a195095187245491cea3bffe442f50227
ZahraAbaszadeh <zahraabaszade96@gmail.com>
2022-06-10 00:38:03 +0430

    Feed completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,32 @@
-import React from "react";
-import { Box } from "@mui/material";
-export default function Feed() {
-  return <Box flex={4} p={2}>Feed</Box>;
-}
+import { Box, Stack, Skeleton } from "@mui/material";
+import React, { useState } from "react";
+import Post from "./Post";
+
+const Feed = () => {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, [3000]);
+
+  return (
+    <Box flex={4} p={{ xs: 0, md: 2 }}>
+      {loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="rectangular" height={300} />
+        </Stack>
+      ) : (
+        <>
+          {Array.from(Array(6)).map((index) => (
+            <Post />
+          ))}
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default Feed;
